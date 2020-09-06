5e05e723832237878f629fe3fcda05c3b5920776
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-09-06 08:29:43 +0500

    Simple Authentication using React and Auth0

---
+++ react-auth0/build/precache-manifest.9c1316718903fcd4d13f434e1d54f2b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b31344f24e2c781a68b6fae91ea7af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9b5d06d3de96bf02265",
+    "url": "/static/css/main.445f4c90.chunk.css"
+  },
+  {
+    "revision": "cc6a73f64a92a2914b1a",
+    "url": "/static/js/2.67945ae7.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.67945ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b5d06d3de96bf02265",
+    "url": "/static/js/main.cbb2e47a.chunk.js"
+  },
+  {
+    "revision": "e15788095190b1c3f1ca",
+    "url": "/static/js/runtime-main.61df71ed.js"
+  }
+]);
